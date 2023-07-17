1f6272410d3d726ac5158f3411dc53b766c19f0e
Ayush Jiwatramani <jiwatramaniayush@gmail.com>
2023-07-17 15:50:53 +0530

    first commit

---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+//database configurations
+define("DB_HOST","localhost");
+define("DB_UNAME","ayush");
+define("DB_PASS","ayush");
+define("DB_DNAME","quizller_v1");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
